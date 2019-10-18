b33be01fe8a15538fc15165cb621e252302a5272
rolandgvc <gavrilescu.roland@gmail.com>
2019-10-18 21:55:53 +0100

    updated

--- deep-reinforcement-learning-nanodegree/p1_navigation/main.py
+++ deep-reinforcement-learning-nanodegree/p1_navigation/main.py
@@ -1,19 +1,11 @@
 import torch
 import numpy as np
 from collections import deque
-import matplotlib.pyplot as plt
+
 from unityagents import UnityEnvironment
+from dqn import DQN
 
-# Environment Setup
-env = UnityEnvironment(file_name="Banana.app")
-brain_name = env.brain_names[0]
-brain = env.brains[brain_name]
-env_info = env.reset(train_mode=True)[brain_name]
-state = env_info.vector_observations[0]
-state_size = len(state)
-action_size = brain.vector_action_space_size
-
-
+# Train function
 def train_dqn(model, n_episodes=2000, max_t=1000, eps_decay=0.995):
     episode_scores = []
     recent_scores = deque(maxlen=100)
@@ -24,7 +16,7 @@ def train_dqn(model, n_episodes=2000, max_t=1000, eps_decay=0.995):
         state = env_info.vector_observations[0]
         score = 0
         
-        for t in range(max_t):
+        for _ in range(max_t):
             action = model.predict(state, eps)
             env_info = env.step(action)[brain_name]        
             next_state = env_info.vector_observations[0]   
@@ -49,23 +41,24 @@ def train_dqn(model, n_episodes=2000, max_t=1000, eps_decay=0.995):
 
         if np.mean(recent_scores) >= 13.0:
             print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(recent_scores)))
-            torch.save(model.local_network.state_dict(), 'dqn_banana_checkpoint.pth')
+            torch.save(model.local_network.state_dict(), 'banana_ckpt.pth')
             break
     
-    return episode_scores
 
+if __name__ == "__main__":
+    # Environment setup
+    env = UnityEnvironment(file_name="Banana.app")
+    brain_name = env.brain_names[0]
+    brain = env.brains[brain_name]
+    env_info = env.reset(train_mode=True)[brain_name]
+    state = env_info.vector_observations[0]
+    state_size = len(state)
+    action_size = brain.vector_action_space_size
+    
+    # Load model
+    model = DQN(state_size=state_size, action_size=action_size, seed=0)        
 
-from dqn import DQN
-model = DQN(state_size=state_size, action_size=action_size, seed=0)        
-scores = train_dqn(model)
-print("scores", scores)
+    # Start training
+    train_dqn(model)
 
-# # plot the scores
-# fig = plt.figure()
-# ax = fig.add_subplot(111)
-# plt.plot(np.arange(len(scores)), scores)
-# plt.ylabel('Score')
-# plt.xlabel('Episode #')
-# plt.show()
-
-env.close()
+    env.close()
